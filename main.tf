d4dfb1cf66a35b3538dea82ea6c56fed71849ea3
Rama Krishna <jvrkrishnaa@gmail.com>
2023-12-14 12:03:15 +0530

    s

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 resource "aws_ssm_parameter" "foo" {
-  name  = "nasreen"
-  type  = "String"
-  value = "nasreen@123"
+  count=length(var.params)
+  name  = var.params[var.params].name
+  type  = var.params[var.params].type
+  value = var.params[var.params].value
+}
+
+variable "params" {
+  default = [
+    { name:"Gopal", type:"String", value:"Gopal@123" },
+    { name:"Seetha", type:"String", value:"Seetha@123" },
+  ]
 }
