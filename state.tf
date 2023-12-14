8f581c20326a3025e35b6083a5965a36afd70a20
Rama Krishna <jvrkrishnaa@gmail.com>
2023-12-14 14:06:47 +0530

    s

--- state.tf
+++ state.tf
@@ -1,8 +1,8 @@
 ############## tfstate backend configuration in terraform--available backends (s3) ################
 terraform {
-backend "s3" {
-bucket = "roboshopterraform-bucket"
-key    = "params/terraform.tfstate"
-region = "us-east-1"
+  backend "s3" {
+    bucket = "roboshopterraform-bucket"
+    key    = "params/terraform.tfstate"
+    region = "us-east-1"
 }
 }
